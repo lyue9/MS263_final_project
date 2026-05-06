fb41e932e2f61261bfa84505c95d78ea21de39cb
Lindsay Yue <lyue@csumb.edu>
2026-05-06 15:42:58 -0700

    Write a descriptive commit message here.

--- Final_dataset.py
+++ Final_dataset.py
@@ -3,16 +3,16 @@
 from skbio.diversity import alpha_diversity
 import pandas as pd
 
-def final_dataset(rarefied_OTUs, metadata):
+def merged_data(rarefied_OTUs, metadata):
     Shannon = alpha_diversity('shannon', rarefied_OTUs)
     Sobs = alpha_diversity('sobs', rarefied_OTUs)
     adiv = pd.concat([Shannon, Sobs], axis=1)
     adiv.columns = ['Shannon', 'Sobs']
 
-    final_data = pd.concat([adiv, metadata], axis=1)
-    final_data.dropna(subset=["Shannon"])
+    merged_data = pd.concat([adiv, metadata], axis=1)
+    merged_data = merged_data.dropna(subset=["Shannon"])
    
-    return final_data
+    return merged_data
 
     '''Input:
        rarefied_OTUs - Rarefied OTU tables. Sample names are in first column, and OTU counts in other columns.  
